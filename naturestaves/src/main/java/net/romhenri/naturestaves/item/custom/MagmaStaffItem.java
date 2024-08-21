7507d0843dce14cd19021a8677b91d8a2cd6f720
Rômulo Henri <romulohenri000@gmail.com>
2024-08-21 20:34:40 -0300

    add: Nature Staves Items Classes

---
+++ naturestaves/src/main/java/net/romhenri/naturestaves/item/custom/MagmaStaffItem.java
@@ -0,0 +1,40 @@
+package net.romhenri.naturestaves.item.custom;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.core.BlockPos;
+import net.romhenri.naturestaves.procedures.MagmaRightClickProcedure;
+
+public class MagmaStaffItem extends Item {
+
+    private static final int COOLDOWN_TICKS = 10;
+    private static final int DURABILITY_COST = 1;
+
+    public MagmaStaffItem(Item.Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        if (world.isClientSide()) {
+            return InteractionResultHolder.pass(player.getItemInHand(hand));
+        }
+
+        if (!(player instanceof ServerPlayer)) {
+            return InteractionResultHolder.pass(player.getItemInHand(hand));
+        }
+
+        ServerPlayer serverPlayer = (ServerPlayer) player;
+        ItemStack itemStack = serverPlayer.getItemInHand(hand);
+        BlockPos blockPos = serverPlayer.blockPosition();
+
+        MagmaRightClickProcedure.execute(world, blockPos, serverPlayer, itemStack, COOLDOWN_TICKS, DURABILITY_COST);
+
+        return InteractionResultHolder.success(itemStack);
+    }
+}
