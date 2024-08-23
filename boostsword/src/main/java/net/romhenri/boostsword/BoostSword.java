afe1141fb3bf8f8f2af34778ef60b722346e8295
Rômulo Henri <romulohenri000@gmail.com>
2024-08-22 21:42:56 -0300

    add: Boost Sword Mod

---
+++ boostsword/src/main/java/net/romhenri/boostsword/BoostSword.java
@@ -0,0 +1,61 @@
+package net.romhenri.boostsword;
+
+import com.mojang.logging.LogUtils;
+import net.romhenri.boostsword.block.ModBlocks;
+import net.romhenri.boostsword.item.ModCreativeModeTabs;
+import net.romhenri.boostsword.loot.ModLootModifiers;
+import net.romhenri.boostsword.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(BoostSword.MOD_ID)
+public class BoostSword {
+    public static final String MOD_ID = "boostsword";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public BoostSword() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        ModLootModifiers.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
