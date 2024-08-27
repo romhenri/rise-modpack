f6d7e3e4ebe54d377ffa85f48d2205dc98da2779
Rômulo Henri <romulohenri000@gmail.com>
2024-08-27 20:07:24 -0300

    add: Alexandrite Hammer Event Subscriber

---
+++ alexandriteore/src/main/java/net/romhenri/alexandriteore/event/ModEvents.java
@@ -0,0 +1,41 @@
+package net.romhenri.alexandriteore.event;
+
+import net.romhenri.alexandriteore.AlexandriteOreMod;
+import net.romhenri.alexandriteore.item.custom.HammerItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Mod.EventBusSubscriber(modid = AlexandriteOreMod.MOD_ID)
+public class ModEvents {
+    private static final Set<BlockPos> HARVESTED_BLOCKS = new HashSet<>();
+    @SubscribeEvent
+    public static void onHammerUsage(BlockEvent.BreakEvent event) {
+        Player player = event.getPlayer();
+        ItemStack mainHandItem = player.getMainHandItem();
+
+        if(mainHandItem.getItem() instanceof HammerItem hammer && player instanceof ServerPlayer serverPlayer) {
+            BlockPos initalBlockPos = event.getPos();
+            if (HARVESTED_BLOCKS.contains(initalBlockPos)) {
+                return;
+            }
+
+            for (BlockPos pos : HammerItem.getBlocksToBeDestroyed(1, initalBlockPos, serverPlayer)) {
+                if(pos == initalBlockPos || !hammer.isCorrectToolForDrops(mainHandItem, event.getLevel().getBlockState(pos))) {
+                    continue;
+                }
+
+                HARVESTED_BLOCKS.add(pos);
+                serverPlayer.gameMode.destroyBlock(pos);
+                HARVESTED_BLOCKS.remove(pos);
+            }
+        }
+    }
+}
