c1b32d92ef9fdda374ec407b7373004e6eb40e5a
Rômulo Henri <romulohenri000@gmail.com>
2024-08-31 22:00:44 -0300

    add: Ruby Creative Tab

---
+++ rubyore/src/main/java/net/romhenri/rubyore/item/ModCreativeModeTabs.java
@@ -0,0 +1,39 @@
+package net.romhenri.rubyore.item;
+
+import net.romhenri.rubyore.RubyOreMod;
+import net.romhenri.rubyore.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RubyOreMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> RUBY_TAB = CREATIVE_MODE_TABS.register("ruby_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.RUBY.get()))
+                    .title(Component.translatable("creativetab.ruby_tab"))
+                    .displayItems((displayParameters, output) -> {
+                        output.accept(ModItems.RUBY.get());
+                        output.accept(ModItems.RUBY_DUST.get());
+
+                        output.accept(ModItems.RUBY_SWORD.get());
+                        output.accept(ModItems.RUBY_PICKAXE.get());
+                        output.accept(ModItems.RUBY_SHOVEL.get());
+                        output.accept(ModItems.RUBY_AXE.get());
+                        output.accept(ModItems.RUBY_HOE.get());
+
+                        output.accept(ModBlocks.RUBY_BLOCK.get());
+
+                        output.accept(ModBlocks.RUBY_ORE.get());
+                        output.accept(ModBlocks.DEEPSLATE_RUBY_ORE.get());
+                    }).build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
