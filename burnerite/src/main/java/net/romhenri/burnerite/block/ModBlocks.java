0825004188e72f04202a89a3289284b09fd23aa6
Rômulo Henri <romulohenri000@gmail.com>
2024-09-10 21:49:27 -0300

    add: Burnerite Blocks

---
+++ burnerite/src/main/java/net/romhenri/burnerite/block/ModBlocks.java
@@ -0,0 +1,46 @@
+package net.romhenri.burnerite.block;
+
+import net.romhenri.burnerite.BurneriteMod;
+import net.romhenri.burnerite.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, BurneriteMod.MOD_ID);
+
+    public static final RegistryObject<Block> BURNERITE_ORE = registerBlock("burnerite_ore",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK)
+                    .strength(50f, 1200f)
+                    .requiresCorrectToolForDrops()));
+
+    private static final Logger log = LoggerFactory.getLogger(ModBlocks.class);
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
