f344818b8b1288cb21ba32773deb9e25803f7d75
Rômulo Henri <romulohenri000@gmail.com>
2024-09-29 18:33:38 -0300

    add: Alpha n Beta Boost Sword Items

--- boostsword/src/main/java/net/romhenri/boostsword/item/ModItems.java
+++ boostsword/src/main/java/net/romhenri/boostsword/item/ModItems.java
@@ -13,9 +13,27 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BoostSword.MOD_ID);
 
-    // Boost Sword
-    public static final RegistryObject<Item> BOOST_SWORD = ITEMS.register("boost_sword",
-            () -> new BoostSwordItem(Tiers.DIAMOND, 3, -2.4F, new Item.Properties().durability(1561)));
+    // Alpha Boost Sword
+    public static final RegistryObject<Item> ALPHA_BOOST_SWORD = ITEMS.register("alpha_boost_sword",
+            () -> new BoostSwordItem(
+                    Tiers.DIAMOND,
+                    3,
+                    -2.4F,
+                    new Item.Properties().durability(1561),
+                    30,
+                    3.0
+            ));
+
+    // Beta Boost Sword
+    public static final RegistryObject<Item> BETA_BOOST_SWORD = ITEMS.register("beta_boost_sword",
+            () -> new BoostSwordItem(
+                    Tiers.DIAMOND,
+                    3,
+                    -2.4F,
+                    new Item.Properties().durability(1561),
+                    30,
+                    1.5
+            ));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
